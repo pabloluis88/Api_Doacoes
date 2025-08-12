a8f3e337ae4cb77ce5070b5a1af24e52d42c3b69
pabloluis88 <pabloleite@outlook.com>
2025-08-12 11:14:22 -0300

    projeto doacoes

---
+++ src/main/java/com/projeto/doacoes/DoacoesApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.doacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoacoesApplication.class, args);
+	}
+
+}
